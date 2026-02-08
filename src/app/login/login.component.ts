549c1f3676d957beff8a284894106762076823bc
alejandro cisneros <alex.cisnerosap@gmail.com>
2026-02-08 18:08:03 +0100

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router'; 
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+import { AuthResponse } from '../modelos/auth-response';
 
 @Component({
   selector: 'app-login',
-  standalone: true, 
-  imports: [RouterLink], 
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {}
+export class LoginComponent {
+  credentials = {
+    username: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.credentials).subscribe({
+      next: (res: AuthResponse) => {
+        console.log('Ninja autenticado', res);
+        this.router.navigate(['/tablon']);
+      },
+      error: (err) => {
+        console.error('Error de acceso', err);
+        alert('Credenciales incorrectas');
+      }
+    });
+  }
 }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Preferences } from '@capacitor/preferences';
+import { AuthResponse } from '../modelos/auth-response';
+import { Ninja } from '../modelos/ninja';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'https://pr3-lista-misiones-konoha-backend.vercel.app/auth';
+  private authStatus = new BehaviorSubject<Ninja|null>(null);
+
+  constructor(private http: HttpClient) {
+  }
+
+  
+  register(datos: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/register`, datos);
+  }
+  login(credentials: any) {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials).pipe(
+      tap(async (res) => {
+        if (res.token) {
+          await Preferences.set({ key: 'token', value: res.token });
+          this.authStatus.next(res.user);
+        }
+      })
+    );
+  }
+
   
 }
